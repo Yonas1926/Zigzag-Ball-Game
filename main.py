5d55935ecb2aa112c87948b1b27ab22d6e75581e
yonas1926 <yonasawraris123@gmail.com>
2022-06-19 14:55:14 +0300

    scoring, game over page and optimization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 import numpy as np
 import pygame
 from pygame.locals import *
@@ -7,6 +7,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from Classes import *
 
+pygame.init()
 background = Background()
 display = (background.display_x, background.display_y)
 
@@ -58,44 +59,60 @@ def draw_game():
     # pygame.draw.circle(win, (255, 0, 0), ball1.hit_area[0], ball1.hit_area[1], 0)
     for obstaclee in obstacles:
         obstaclee.draw(win)
+    text_font = pygame.font.SysFont("comicsans", 18)
+    text = text_font.render("Score: " + str(score), True, (255, 255, 255))
+    win.blit(text, (10, 10))
     pygame.display.update()
 
 
-def main():
-    pass
+def gameOverScreen():
+    global obstacles, score, best_score, start
+    obstacles = []
+
+    run = True
+    while run:
+        for this_event in pygame.event.get():
+            if this_event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+        win.fill((255, 255, 255))
+        win.blit(background_Img, (0, 0))
+        text = pygame.font.SysFont("comicsans", 40)
+        bestScore = text.render("Best Score: " + str(best_score), True, (255, 255, 255))
+        win.blit(bestScore, ((display[1]/2 - bestScore.get_width()/2), 120))
+        newScore = text.render("Score: " + str(score), True, (255, 255, 255))
+        win.blit(newScore, ((display[1] / 2 - newScore.get_width() / 2), 270))
+        pygame.display.update()
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[pygame.K_SPACE]:
+            start = time.time()
+            run = False
+    best_score = score
+    score = 0
+
+
+def generateObstacle():
+    obstacle1 = Obstacle(random.randrange(110, 340), -50, 50, 50)
+    obstacle2 = Obstacle2(random.randrange(110, 340), -50, 50, 50)
+    obstacle_list = [obstacle1, obstacle2]
+    rand_obstacle_index = random.randrange(0, len(obstacle_list))
+    obstacles.append(obstacle_list[rand_obstacle_index])
+
 
 
 pygame.time.set_timer(USEREVENT+1, random.randrange(1000, 2000))
 
 obstacles = []
-
-while True:
-    for obstacle in obstacles:
-        if obstacle.collide(ball1.hit_area):
-            pygame.time.delay(100)
-            # print("Collided")
-
-        obstacle.y += 1.4
-        if obstacle.y < obstacle.height * -1:
-            obstacles.pop(obstacles.index(obstacle))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        if event.type == USEREVENT+1:
-            r = random.randrange(0, 2)    # To create obstacle 1 or 2
-            if r == 0:
-                obstacles.append(Obstacle(random.randrange(110, 340), -50, 50, 50))
-            else:
-                obstacles.append(Obstacle2(random.randrange(110, 340), -50, 50, 50))
+best_score = 0
+start = time.time()
+score = 0
 
 
-# code for pressing key functionality
+def key_functionality():
+    global obstacles, ball1, start
+    # code for pressing key functionality
     pressed_key = pygame.key.get_pressed()
-
     # The -5 in the line below is for some gap between the line and the ball
-
     if pressed_key[pygame.K_RIGHT] and ball1.x_position < (background.line2_x_position - ball1.radius - 5):
         ball1.x_position += ball1.speed
         ball1.hit_area[0][0] += ball1.speed
@@ -108,9 +125,38 @@ while True:
     if pressed_key[pygame.K_DOWN] and ball1.y_position < (ball1.display_y - ball1.radius - 5):
         ball1.y_position += ball1.speed
         ball1.hit_area[0][1] += ball1.speed
+        # To restart the game
+    if pressed_key[pygame.K_SPACE]:
+        obstacles = []
+        ball1 = Ball()
+        start = time.time()
+
+
+def motion_of_obstacles_and_check_collision():
+    difficulty = 1 + (score / 5)
+
+    for obstacle in obstacles:
+        if obstacle.collide(ball1.hit_area):
+            pygame.time.delay(1000)
+            gameOverScreen()
+        obstacle.y += difficulty
+        if obstacle.y < obstacle.height * -1:
+            obstacles.pop(obstacles.index(obstacle))
+
+
+while True:
+    score = int(time.time() - start)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if event.type == USEREVENT+1:
+            generateObstacle()
 
     draw_game()
-
+    key_functionality()
+    motion_of_obstacles_and_check_collision()
     pygame.display.flip()
     pygame.time.wait(10)
 
